d0312b9c41d89d43e9b076d5d35ea07332e3761a
Nicola Erario <nicola.erario@gmail.com>
2020-07-10 10:38:48 +0200

    Added Behavior to Image Component with Class
    - Learned the use of 'generate-class';
    - learned the short hand to inizialize an instance variable;
    - learned to monitor the value of an instanced variable with '@tracked';
    - learned to use a method as a callback for html events with '@action';
    - learned how access variables from template with conditional syntax;
    - added tests

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { render, click } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 
 module("Integration | Component | rental/image", function (hooks) {
@@ -19,5 +19,28 @@ module("Integration | Component | rental/image", function (hooks) {
       .dom(".image img")
       .hasAttribute("src", "/assets/images/teaching-tomster.png");
     assert.dom(".image img").hasAttribute("alt", "Teaching Tomster");
+  });
+
+  test("clicking on the component toggle its size", async function (assert) {
+    await render(hbs`
+    <Rental::Image
+    src='/assets/teaching-tomster.png'
+    alt='Teaching Tomster'
+    />
+    `);
+    assert.dom("button.image").exists();
+
+    assert.dom(".image").doesNotHaveClass("large");
+    assert.dom(".image small").hasText("View Larger");
+
+    await click("button.image");
+
+    assert.dom(".image").hasClass("large");
+    assert.dom(".image small").hasText("View Smaller");
+
+    await click("button.image");
+
+    assert.dom(".image").doesNotHaveClass("large");
+    assert.dom(".image small").hasText("View Larger");
   });
 });
